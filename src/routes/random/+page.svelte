202f04de8518c424bfe64c30252dd93082ea31b8
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2024-05-02 17:01:56 +0800

    test

---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Hero from "../../components/Hero.svelte";
+</script>
+
+<main class="flex flex-col">
+    <Hero />
+    <p>goodbye</p>
+    asodaod
+    <div
+        class="alert alert-secondary alert-dismissible fade show"
+        role="alert"
+    >
+        <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+        ></button>
+    
+        <strong><a
+            href="#"
+            class="btn btn-primary active"
+            role="button"
+            >Button</a
+        >
+        </strong> You should check in on some of
+        those fields below.
+    </div>
+    
+</main>
