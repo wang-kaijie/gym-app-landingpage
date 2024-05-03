a00a2ef33d5fb67b04fe1b4772222f8dcc75268f
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2024-05-03 09:09:12 +0800

    commiting

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -3,30 +3,6 @@
 </script>
 
 <main class="flex flex-col">
-    <Hero />
-    <p>goodbye</p>
-    asodaod
-    <div
-        class="alert alert-secondary alert-dismissible fade show"
-        role="alert"
-    >
-        <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="alert"
-            aria-label="Close"
-        ></button>
-    <p></p>
-    <small class="text-muted">Help Text</small>
-    
-        <strong><a
-            href="#"
-            class="btn btn-primary active"
-            role="button"
-            >Button</a
-        >
-        </strong> You should check in on some of
-        those fields below.
-    </div>
-    
+    <Hero />   
+    <p>hello world</p>
 </main>
